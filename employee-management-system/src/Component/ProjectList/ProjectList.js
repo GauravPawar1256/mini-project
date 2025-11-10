4dbd20b03c19412cb252a61836abf0c868576c30
gaurav.pawar <pawargaurav1256@gmail.com>
2025-11-10 18:53:46 +0530

    ui complete

---
+++ employee-management-system/src/Component/ProjectList/ProjectList.js
@@ -0,0 +1,123 @@
+export default function ProjectList() {
+    return (
+        <div className="m-0 bg-slate-200 min-h-screen ">
+            <div className="bg-gray-800 text-white flex justify-between px-8 py-4 items-center ">
+                <div className="text-2xl font-bold">Projects</div>
+                <div>
+                    <input placeholder="Search Project" className="rounded-md p-1 border-l-0 rounded-r-none" />
+                    <button className="bg-white text-black font-semibold p-1 rounded-md border-l-0 rounded-l-none">Search</button>
+                    <button className="bg-white text-black font-semibold p-1 rounded-md mx-2">All Employee</button>
+                    <button className="bg-white text-black font-semibold p-1 rounded-md mx-2">Add Employee</button>
+                    <button className="bg-white text-black font-semibold p-1 rounded-md  mx-2">Add ProjectList</button>
+                </div>
+            </div>
+            <div className=" flex flex-wrap gap-4 p-4  justify-center">
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    {/* <div className="h-40 rounded shadow p-4 min-w-0"> */}
+                    <div className="h-[100px] overflow-y-scroll scrollbar-thin scrollbar-thumb-red-800 scrollbar-track-blue">
+                        <div className=" text-xl font-semibold m-0 break-words">Project Name</div>
+                        <div className="m-2 break-words">About Project kdhgjfhg jebhdvghwcdvyggggggggggggggggggggggggggggggggggggg gggggggggggggggggggggggggggggggggggggggggggggggggggggggggg</div>
+                    </div>
+                    <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-2">Assinged Employee</button></div>
+                    {/* </div> */}
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+                <div className=" bg-gray-800 rounded-md shadow-lg shadow-red-500/50 ... text-center text-white w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2">
+                    <div className="h-40 rounded shadow p-4 min-w-0">
+                        <div className=" text-xl font-semibold m-2">Project Name</div>
+                        <div className="m-2">About Project</div>
+                        <div><button className="bg-white text-gray-800 px-5 py-1 rounded-md items-center m-4s">Assinged Employee</button></div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    );
+}
